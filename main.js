b728cdea8edd6343777d919efb66fc304ccb46b6
GavinHanHAHS <55220804+GavinHanHAHS@users.noreply.github.com>
2020-09-18 15:03:57 -0600

    Finish bullets up to weird bug

--- main.js
+++ main.js
@@ -15,8 +15,29 @@ let player = {
   xSpd: 0
 }
 
-let keys = [];
+// Bullet array
+let bullets = [];
 
+for(let i = 0; i < 9; i++) {
+//  addBullet((i * 50) + 50, 50, 5, player, 3);
+  
+  bullets.push([50 * (i + 1), 50, 5, 0, 0, 3]);
+}
+
+setTimeout(() => setBulletDirection(), 1000);
+setInterval(() => {
+//  for(let i = 0; i < 4; i++) {
+  let angle = Math.atan2(player.y - 150, player.x - 150)
+  console.log(angle);
+  
+  addBullet(150, 150, 5, player, 3);
+  // setTimeout(() => addBullet(150, 150, 5, angle, 3), 100);
+  // setTimeout(() => addBullet(150, 150, 5, angle, 3), 200);
+  // setTimeout(() => addBullet(150, 150, 5, angle, 3), 300);
+//  }
+}, 1500);
+
+let keys = [];
 
 
 requestAnimationFrame(main);
@@ -26,6 +47,8 @@ function main() {
   ctx.fillRect(0, 0, cnv.width, cnv.height); // Reset grid drawing to draw next frame
 
   playerMovement();
+  
+  bulletLogic();
 
   requestAnimationFrame(main);
 }
@@ -69,10 +92,63 @@ function playerMovement() {
   if(keys.includes("ShiftLeft")) {
     ctx.fillStyle = "white";
     fillCircle(player.x, player.y, 3);
+  }  
+}
+
+function bulletLogic() {
+  ctx.fillStyle = "white"
+  // Draw bullets
+  for(let i = 0; i < bullets.length; i++) {
+    fillCircle(bullets[i][0], bullets[i][1], bullets[i][2]);
+    strokeCircle(bullets[i][0], bullets[i][1], bullets[i][2]);
+    bullets[i][0] += bullets[i][3] * bullets[i][5];
+    bullets[i][1] += bullets[i][4] * bullets[i][5];
+
+    if(bullets[i][1] > cnv.height + 5) {
+      bullets.splice(i, 1);
+      i--;
+    } else if(bullets[i][0] > cnv.width + 5) {
+      bullets.splice(i, 1);
+      i--;
+    }
   }
+
+  // Draw basic enemy
+  fillCircle(150, 150, 10);
+  strokeCircle(150, 150, 10);
+
   
 }
 
+function setBulletDirection() {
+  console.log("yeet");
+
+  for(let i = 0; i < bullets.length; i++) {
+    bullets[i][3] = Math.cos(Math.atan2(player.y - bullets[i][1], player.x - bullets[i][0]));
+    bullets[i][4] = Math.sin(Math.atan2(player.y - bullets[i][1], player.x - bullets[i][0]));
+  }
+}
+
+function addBullet(x, y, r, angle, spd) {
+  console.log(angle);
+  let xAngle = 0;
+  let yAngle = 0;
+  if(angle == player) {
+    xAngle = Math.cos(Math.atan2(player.y - y, player.x - x));
+    yAngle = Math.sin(Math.atan2(player.y - y, player.x - x));
+  } else {
+    xAngle = Math.cos(toRadians(angle));
+    yAngle = Math.sin(toRadians(angle));
+  }
+  console.log(xAngle + ", " + yAngle);
+  bullets.push([x, y, r, xAngle, yAngle, spd]);
+}
+
+function addMultipleBullets(x, y) {
+  
+}
+
+// Math Helper Functions
 function constrain(min, max, int) {
   // returns a number constrained in min and max
   if(int > max) {
@@ -82,6 +158,14 @@ function constrain(min, max, int) {
   } else {
     return int;
   }
+}
+
+function toDegrees(radians) {
+  return radians * (180 / Math.PI);
+}
+
+function toRadians(degrees) {
+  return degrees / (180 / Math.PI);
 }
 
 // Event Listeners and Functions
